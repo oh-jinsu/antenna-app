75de0adecd33649555bb3e388a9519d41e670fe9
오진수 <ohjinsu@codersproduct.com>
2022-11-24 14:05:15 +0900

    Update the logger

--- lib/debug/report.dart
+++ lib/debug/report.dart
@@ -1,7 +1,11 @@
+import 'dart:developer';
+
 import 'package:flutter/foundation.dart';
 
 void report(Object object) {
   if (kDebugMode) {
-    print(object);
+    final now = DateTime.now().toIso8601String().split("T")[1];
+
+    log("[$now] $object");
   }
 }
