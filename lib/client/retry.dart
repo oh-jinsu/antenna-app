fe50fb3013281f5309bd6778b013c6164e8bbeb5
오진수 <ohjinsu@codersproduct.com>
2023-04-13 11:03:56 +0900

    Retry once if status code is 500

--- lib/client/retry.dart
+++ lib/client/retry.dart
@@ -8,6 +8,14 @@ Future<Response> retry(Future<Response> Function() fn, {int count = 5}) async {
     return response;
   }
 
+  if (response.statusCode == 500) {
+    report("Response status code is 500. Retry once ...");
+
+    await Future.delayed(const Duration(milliseconds: 5000));
+
+    return retry(fn, count: 0);
+  }
+
   if (response.statusCode == 429 ||
       response.statusCode == 501 ||
       response.statusCode == 502 ||
